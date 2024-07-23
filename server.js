79ec9b9341af898162f73bf1cd10b31943df799c
j b <j.is.brown@gmail.com>
2024-07-23 16:54:44 -0500

    added inquirerer and mysql2 requirements in server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const inquirer = require("inquirer");
+const mysql = require('mysql2');
+
+
+const connection = mysql.createConnection({
+    host 'localhost',
+    port: 3001,
+    user: 'postgres',
+    password: 'The6th287!',
+    database: 'employeeTracker_db',
+});
