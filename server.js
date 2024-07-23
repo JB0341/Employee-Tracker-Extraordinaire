d1d73b2cf622a9c8d9c2aaba456217453adfab3f
j b <j.is.brown@gmail.com>
2024-07-23 17:17:13 -0500

    added function to start employee tracker

--- server.js
+++ server.js
@@ -15,3 +15,51 @@ connection.connect((err) => {
     start();
 });
 
+function start() {
+    inquirer
+        .createPromptModule({
+            type: 'list',
+            name: 'action',
+            message: 'what would you like to do?'
+            choices: [
+                'View Departments',
+                'View Roles',
+                'View Employees',
+                'Add Department',
+                'Add Role',
+                'Add Employee',
+                'Update Role',
+                'Exit',
+            ],
+        })
+        .then((answer) => {
+            switch (answer.action) {
+                case 'View departments':
+                    viewDepartments();
+                    break;
+                case 'View Roles':
+                    viewRoles();
+                    break;
+                case 'View Employees':
+                    viewEmployees();
+                    break;
+                case 'Add Department':
+                    addDepartment();
+                    break;
+                case 'Add Role':
+                    addRole();
+                    break;
+                case 'Add Employee':
+                    addEmployee();
+                    break;
+                case 'Update Employee Role':
+                    updateEmployeeRole();
+                case 'Exit':
+                    connection.end();
+                    break;
+            }
+        });
+}
+process.on('exit', () => {
+    connection.end();
+});
